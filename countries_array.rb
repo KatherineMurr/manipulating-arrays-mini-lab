a39cf39419ff94d4385bc1dad2df7e49088157e5
Katherine Murr <katherinemurr@yahoo.com>
2017-06-07 21:25:47 +0000

    countries lab with kwk 2017

--- countries_array.rb
+++ countries_array.rb
@@ -1,1 +1,6 @@
+four_letter_countries = ["chad", "cuba", "greenland", "iraq", "mali", "oman", "india"]
+four_letter_countries.push("fiji")
+four_letter_countries.insert(4, "iran")
+four_letter_countries.delete("india")
+puts four_letter_countries.reverse
 
